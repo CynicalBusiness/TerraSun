dba9a6bd70e4ade42d5abb1caf0bfd981ee67337
Matthew Struble <Matt@MayateckInnovations.net>
2014-09-05 21:08:15 -0700

    Added creature spawn reduction at night.

--- src/me/capit/TerraSun/WeatherEventHandler.java
+++ src/me/capit/TerraSun/WeatherEventHandler.java
@@ -2,6 +2,8 @@ package me.capit.TerraSun;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.event.weather.WeatherChangeEvent;
 
 public class WeatherEventHandler implements Listener {
@@ -19,6 +21,15 @@ public class WeatherEventHandler implements Listener {
 		boolean doweather = plugin.getConfig().getBoolean("config.DO_WEATHER");
 		if (chance>rainChance && doweather){
 			e.setCancelled(true);
+		}
+	}
+	
+	@EventHandler
+	public void onMobSpawn(CreatureSpawnEvent e){
+		if (e.getSpawnReason()==SpawnReason.NATURAL){
+			if (Math.random()>0.25){
+				e.setCancelled(true);
+			}
 		}
 	}
 	
